d1804750ce0fb3ee63eb7c43960ed38cb893e97c
Sean Blessing <snblessing@gmail.com>
2025-06-12 16:49:42 -0400

    demo getting property of actor

--- 04-ts-demos/ts/movie-app.ts
+++ 04-ts-demos/ts/movie-app.ts
@@ -1,8 +1,10 @@
 import { Actor } from "./actor";
 
-
 console.log("Welcome to the movie app!");
-let a: Actor = new Actor(1, "Johnny", "Depp", new Date(1963, 5, 9), 'Male');
+let a: Actor = new Actor(1, "Johnny", "Depp", new Date(1963, 5, 9), "Male");
+
+console.log("actor id: " + a.id);
+
 console.log(a.toString());
 
 console.log("Bye");
